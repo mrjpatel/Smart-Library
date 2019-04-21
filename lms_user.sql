da76f8ebf225d0603dcedb01df5cac56a379bae8
Toan Nguyen <s3604969@student.rmit.edu.au>
2019-04-21 19:08:12 +1000

    Add email to user table

--- lms_user.sql
+++ lms_user.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS lms_user (
     encrypted_password TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
+    email TEXT NOT NULL,
     CONSTRAINT pk_lms_user PRIMARY KEY (user_id),
     CONSTRAINT un_username UNIQUE (username)
+    CONSTRAINT un_email UNIQUE (email)
 );

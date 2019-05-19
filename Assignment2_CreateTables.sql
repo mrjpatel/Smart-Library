06efad5dbaf74277a7b7685ea84f368ac8f3b713
rjmenezes <s3600950@student.rmit.edu.au>
2019-05-19 19:45:44 +1000

    Updated Table schemas

--- Assignment2_CreateTables.sql
+++ Assignment2_CreateTables.sql
@@ -1,11 +1,9 @@
 CREATE TABLE IF NOT EXISTS LmsUser (
-    user_id INTEGER NOT NULL auto_increment,
     username NVARCHAR(256) NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL,
-    CONSTRAINT PK_LmsUser PRIMARY KEY (user_id)
-    CONSTRAINT UN_UserName unique (username)
+    CONSTRAINT PK_LmsUser PRIMARY KEY (username)
 );
 
 create table Book (
@@ -18,12 +16,12 @@ create table Book (
 
 create table BookBorrowed (
     BookBorrowedID int not null auto_increment,
-    LmsUserID int not null,
+    LmsUsername NVARCHAR(256) not null,
     BookID int not null,
     Status enum ('borrowed', 'returned'),
     BorrowedDate date not null,
     ReturnedDate date null,
     constraint PK_BookBorrowed primary key (BookBorrowedID),
-    constraint FK_BookBorrowed_LmsUser foreign key (LmsUserID) references LmsUser (user_id),
+    constraint FK_BookBorrowed_LmsUser foreign key (LmsUsername) references LmsUser (username),
     constraint FK_BookBorrowed_Book foreign key (BookID) references Book (BookID)
 );

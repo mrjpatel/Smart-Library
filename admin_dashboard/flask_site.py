10ae6e604dbfcd817019afbf77c9306fd3c55265
Japan Patel <japanpatel1198@gmail.com>
2019-05-23 13:38:58 +1000

    Update route for login site

--- admin_dashboard/flask_site.py
+++ admin_dashboard/flask_site.py
@@ -18,17 +18,6 @@ def index():
     return render_template("index.html", books=data)
 
 
-# Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
-
 # Client webpage.
 @site.route("/login")
 def index():

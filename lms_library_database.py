c43c0321c0ebac576a3dc99ba2ef7bd831b0d98d
Toan Nguyen <s3604969@student.rmit.edu.au>
2019-05-25 21:26:54 +1000

    Case insensitive book search

--- lms_library_database.py
+++ lms_library_database.py
@@ -101,7 +101,7 @@ class LMSLibraryDatabase:
         """
         # prepare statement
         query = """SELECT * FROM Book
-                    WHERE Author = %(author)s;"""
+                    WHERE LOWER(Author) = LOWER(%(author)s);"""
         # sanitize inputs
         params = {
             "author": author
@@ -120,7 +120,7 @@ class LMSLibraryDatabase:
         """
         # prepare statement
         query = """SELECT * FROM Book
-                    WHERE Title = %(title)s;"""
+                    WHERE LOWER(Title) = LOWER(%(title)s);"""
         # sanitize inputs
         params = {
             "title": title

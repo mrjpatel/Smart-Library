24dc348869aa29de72f9f51d88f9cf396f7090ca
Toan Nguyen <s3604969@student.rmit.edu.au>
2019-05-26 21:23:15 +1000

    Add console register, console login tests

--- library/tests/test_console_login.py
+++ library/tests/test_console_login.py
@@ -5,10 +5,22 @@ from library.reception.console_login import ConsoleLogin
 
 
 class Test_ConsoleLogin(unittest.TestCase):
-    def testValidateUsername(self):
-        with patch("builtins.input", side_effect="user1"):
+    def test_valid_username(self):
+        user_input = [
+            "user1"
+        ]
+        with patch("builtins.input", side_effect=user_input):
             cl = ConsoleLogin("")
             username = cl.get_username()
+            self.assertEqual(username, "user1")
+
+    def test_valid_password(self):
+        user_input = [
+            "password123"
+        ]
+        with patch("builtins.input", side_effect=user_input):
+            cl = ConsoleLogin("")
+            username = cl.get_password()
             self.assertEqual(username, "user1")
 
 

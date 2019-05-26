f0141d93e54344d53152aa2c14e7bf69c3019b8b
Japan Patel <japanpatel1198@gmail.com>
2019-05-26 14:48:00 +1000

    Added image encoding

--- image_encoding.py
+++ image_encoding.py
@@ -4,9 +4,9 @@
 # OR specifying the dataset, encodings and detection method
 #         python3 02_encode.py -i dataset -e encodings.pickle -d hog
 
-## Acknowledgement
-## This code is adapted from:
-## https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/
+# Acknowledgement
+# This code is adapted from:
+# https://www.pyimagesearch.com/2018/06/18/face-recognition-with-opencv-python-and-deep-learning/
 
 # import the necessary packages
 from imutils import paths
@@ -16,51 +16,40 @@ import pickle
 import cv2
 import os
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", default = "dataset",
-    help="path to input directory of faces + images")
-ap.add_argument("-e", "--encodings", default = "encodings.pickle",
-    help="path to serialized db of facial encodings")
-ap.add_argument("-d", "--detection-method", type = str, default = "hog",
-    help="face detection model to use: either `hog` or `cnn`")
-args = vars(ap.parse_args())
 
-# grab the paths to the input images in our dataset
-print("[INFO] quantifying faces...")
-imagePaths = list(paths.list_images(args["dataset"]))
+class ImageEncoding:
 
-# initialize the list of known encodings and known names
-knownEncodings = []
-knownNames = []
+    def encode(self, encoding_file):
+        print("Encoding registered face...")
 
-# loop over the image paths
-for (i, imagePath) in enumerate(imagePaths):
-    # extract the person name from the image path
-    print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
-    name = imagePath.split(os.path.sep)[-2]
+        imagePaths = list(paths.list_images("dataset"))
 
-    # load the input image and convert it from RGB (OpenCV ordering)
-    # to dlib ordering (RGB)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        knownEncodings = []
+        knownNames = []
 
-    # detect the (x, y)-coordinates of the bounding boxes
-    # corresponding to each face in the input image
-    boxes = face_recognition.face_locations(rgb, model = args["detection_method"])
+        for (i, imagePath) in enumerate(imagePaths):
+            print("Processing image {}/{}".format(i + 1, len(imagePaths)))
+            name = imagePath.split(os.path.sep)[-2]
 
-    # compute the facial embedding for the face
-    encodings = face_recognition.face_encodings(rgb, boxes)
-    
-    # loop over the encodings
-    for encoding in encodings:
-        # add each encoding + name to our set of known names and encodings
-        knownEncodings.append(encoding)
-        knownNames.append(name)
+            image = cv2.imread(imagePath)
+            rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# dump the facial encodings + names to disk
-print("[INFO] serializing encodings...")
-data = { "encodings": knownEncodings, "names": knownNames }
+            boxes = face_recognition.face_locations(rgb, model="hog")
+            encodings = face_recognition.face_encodings(rgb, boxes)
 
-with open(args["encodings"], "wb") as f:
-    f.write(pickle.dumps(data))
+            for encoding in encodings:
+                knownEncodings.append(encoding)
+                knownNames.append(name)
+
+        print("Serializing encodings...")
+        data = {
+            "encodings": knownEncodings,
+            "names": knownNames
+            }
+
+        with open(encoding_file, "wb") as f:
+            f.write(pickle.dumps(data))
+
+if __name__ == "__main__":
+    ie = ImageEncoding()
+    print(ie.register("encodings.pickle"))
